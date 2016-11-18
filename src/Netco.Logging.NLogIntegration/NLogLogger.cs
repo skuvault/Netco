0a4f0a3b4f52dbe3fa77db0929d32d4e6263785d
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2016-11-18 12:50:19 +0300

    Fixed warnings for nlog

--- src/Netco.Logging.NLogIntegration/NLogLogger.cs
+++ src/Netco.Logging.NLogIntegration/NLogLogger.cs
@@ -4,7 +4,7 @@ using NLog;
 
 namespace Netco.Logging.NLogIntegration
 {
-	internal class NLogLogger : ILogger
+	internal class NLogLogger: ILogger
 	{
 		private readonly Logger _logger;
 
@@ -20,7 +20,7 @@ namespace Netco.Logging.NLogIntegration
 
 		public void Trace( Exception exception, string message )
 		{
-			this._logger.Trace( message, exception );
+			this._logger.Trace( exception, message );
 		}
 
 		public void Trace( string format, params object[] args )
@@ -30,7 +30,7 @@ namespace Netco.Logging.NLogIntegration
 
 		public void Trace( Exception exception, string format, params object[] args )
 		{
-			this._logger.Trace( string.Format( format, args ), exception );
+			this._logger.Trace( exception, string.Format( format, args ) );
 		}
 
 		public void Debug( string message )
@@ -40,7 +40,7 @@ namespace Netco.Logging.NLogIntegration
 
 		public void Debug( Exception exception, string message )
 		{
-			this._logger.Debug( message, exception );
+			this._logger.Debug( exception, message );
 		}
 
 		public void Debug( string format, params object[] args )
@@ -50,7 +50,7 @@ namespace Netco.Logging.NLogIntegration
 
 		public void Debug( Exception exception, string format, params object[] args )
 		{
-			this._logger.Debug( string.Format( format, args ), exception );
+			this._logger.Debug( exception, string.Format( format, args ) );
 		}
 
 		public void Info( string message )
@@ -60,7 +60,7 @@ namespace Netco.Logging.NLogIntegration
 
 		public void Info( Exception exception, string message )
 		{
-			this._logger.Info( message, exception );
+			this._logger.Info( exception, message );
 		}
 
 		public void Info( string format, params object[] args )
@@ -70,7 +70,7 @@ namespace Netco.Logging.NLogIntegration
 
 		public void Info( Exception exception, string format, params object[] args )
 		{
-			this._logger.Info( string.Format( format, args ), exception );
+			this._logger.Info( exception, string.Format( format, args ) );
 		}
 
 		public void Warn( string message )
@@ -80,7 +80,7 @@ namespace Netco.Logging.NLogIntegration
 
 		public void Warn( Exception exception, string message )
 		{
-			this._logger.Warn( message, exception );
+			this._logger.Warn( exception, message );
 		}
 
 		public void Warn( string format, params object[] args )
@@ -90,7 +90,7 @@ namespace Netco.Logging.NLogIntegration
 
 		public void Warn( Exception exception, string format, params object[] args )
 		{
-			this._logger.Warn( string.Format( format, args ), exception );
+			this._logger.Warn( exception, string.Format( format, args ) );
 		}
 
 		public void Error( string message )
@@ -100,7 +100,7 @@ namespace Netco.Logging.NLogIntegration
 
 		public void Error( Exception exception, string message )
 		{
-			this._logger.Error( message, exception );
+			this._logger.Error( exception, message );
 		}
 
 		public void Error( string format, params object[] args )
@@ -110,7 +110,7 @@ namespace Netco.Logging.NLogIntegration
 
 		public void Error( Exception exception, string format, params object[] args )
 		{
-			this._logger.Error( string.Format( format, args ), exception );
+			this._logger.Error( exception, string.Format( format, args ) );
 		}
 
 		public void Fatal( string message )
@@ -120,7 +120,7 @@ namespace Netco.Logging.NLogIntegration
 
 		public void Fatal( Exception exception, string message )
 		{
-			this._logger.Fatal( message, exception );
+			this._logger.Fatal( exception, message );
 		}
 
 		public void Fatal( string format, params object[] args )
@@ -130,14 +130,14 @@ namespace Netco.Logging.NLogIntegration
 
 		public void Fatal( Exception exception, string format, params object[] args )
 		{
-			this._logger.Fatal( string.Format( format, args ), exception );
+			this._logger.Fatal( exception, string.Format( format, args ) );
 		}
 	}
 
 	/// <summary>
 	/// Returns logger
 	/// </summary>
-	public class NLogLoggerFactory : ILoggerFactory
+	public class NLogLoggerFactory: ILoggerFactory
 	{
 		private readonly Dictionary< Type, ILogger > _typeLoggers = new Dictionary< Type, ILogger >();
 		private readonly Dictionary< string, ILogger > _loggers = new Dictionary< string, ILogger >();
